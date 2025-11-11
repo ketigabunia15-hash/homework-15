b414736bda349d90798c13d9ac6786ed0b669e2c
ketigabunia15-hash <ketigabunia15@gmail.com>
2025-11-11 10:30:37 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
  console.log('Hello, World!');
  console.log('Hello, World!',  'Hello, World!');
 
+let name="Ketevani";
+let age=23;
+let dayofweek="Monday";
+let favoriteactivity="singing";
+
+console.log("Hello, my name is " + name + ", I am " + age + " years old, today is " + dayofweek + " and my favorite activity is " + favoriteactivity + ".");
